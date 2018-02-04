f2b5d9d19c22d94d45b658c3a6334094c58bb199
unknown <sobatsubihi@163.com>
2018-02-04 21:57:17 +0800

    first commit

---
+++ my-project/src/components/index.vue
@@ -0,0 +1,644 @@
+<template>
+  <div id="wrap">
+	  	<div id="header">
+	  		<h1>Lord Xue Ying</h1>
+	  	</div>
+  	<!--弹窗选项-->
+  	<div id="Popup" v-show="isShow">
+  		<div id="previous"><span>previous</span></div>
+  		<div id="menu" @click="catalog"><span>menu</span></div>
+  		<div id="next"><span>next</span></div>
+  	</div>
+  	  		<!--弹窗目录-->
+  		<div id="Catalog-p" v-show="iscatalog">
+  			<p>Chapter 1: Snow Eagle Territory</p>
+  			<p>Chapter 2: Transcendent</p>
+  			<p>Chapter 3: Seperation</p>
+  			<p>Chapter 4: Brothers</p>
+  		</div>
+  	<div id="main-wrap">
+				<div id="header-next">
+		  		<div id="novel-cover"><img src="../../static/images/1516779207(1).jpg"/></div>
+		  		<div id="h3"><h3>Lord Xue Ying</h3></div>
+		  		<div id="author"><span>Author: I Eat Tomatoes</span></div>
+		  		<div id="introduce">
+		  			<p>In the Tranquil Sun province of the empire, there exists an unremarkable lordship known as Xue Ying Territory! This is the home of the Dong Bo clan, the clan of our hero Xue Ying!His father, a commoner turned noble; his mother, a noble who abandoned her clan for love, and his brother, an innocent toddler. But peace cannot last forever, Xue Ying’s peaceful life is shattered, and the only way to reclaim it is through power!</p>
+		  		</div>
+		  	</div>
+  		<div id="Catalog"><h4>Chapter 1: Snow Eagle Territory</h4></div>
+  		<div id="main-up">
+  			<p @click="toggle">
+  				
+The boy hoisted up the short spear. His body tilted slightly backwards, strength flowing from his waist to his right arm as he threw the spear!
+
+
+
+Whoosh!
+
+
+
+The short spear whistled through the air, piercing a few tree leaves as it covered a distance of 30 meters, just barely scraping past the rear of the wild deer. Only a speck of blood remained on the deer’s butt after it landed in the snow. The wild deer accelerated with all of its might towards the depths of the mountains. But just as it was about to run out of sight…
+
+
+
+A rock flew by.
+
+
+
+The rock flew through the through the mountain forest like a laser, instantly closing the 100 meter gap. With a thump, it pierced through the trunk of a large tree and then the deer’s skull. The wild deer swayed from side to side as its momentum carried it forward ten more meters before crashing into the ground.
+
+
+
+"Father!" The boy turned his head and looked into the distance. "Don’t help me. A little more and I would have got it."
+
+
+
+"That deer would have escaped if I didn’t help. Your spear’s accuracy while running is still inadequate. Tonight, go back and practice using the spear another 500 times." The forceful voice resonated from far away. Two figures appeared in the distance..
+
+
+
+One was a rather sturdy middle-aged man with black hair and black eyes who carried a box of weapons on his back. The other was even bulkier, towering at over two meters in height. His arms were thicker than human thighs, his head was that of a lion. He truly did have a lion’s head and a human’s body! His messy blonde hair clearly marked him as one of the seldom seen ‘Lionmen’ of the beastman race. He also carried a box.
+
+
+
+The middle-aged man smiled, "Tong San, old friend, you see how great my son is? He’s only eight yet he’s already as strong as a grown man."
+
+
+
+"Mn. Xue Ying is not bad. It won’t be a problem for him to surpass you in the future," Tong San teased.
+
+
+
+"Of course he’ll be stronger than me. When I was eight, I was still laughing around and playing with the village’s children. I didn’t understand anything at all. I only got a chance to start cultivating qi after entering the army!" The middle-aged man sighed in regret. "I may not be able to provide my son with the best conditions for training, but as his father, I will spare no effort to raise him properly!"
+
+
+
+"Dong Bo, it’s already impressive that you’ve become a Heaven Rank Knight after starting off as a commoner and purchased your own territory to become a lord." The brawny lion-man smiled.
+
+
+
+This middle-aged male was the lord of a territory of more than 100 square kilometers: Baron Dong Bo Lie!
+
+
+
+Baron was the lowest title of nobility in the Xia Clan’s Mountain Dragon Empire. When the empire had first been established, the awarding of noble titles was very strict. The current empire had already existed for 9000 years. This goliath had now fallen to the point where the government permitted low rank noble titles to be bought and sold.
+
+
+
+Dong Bo Lie and his wife had decided to buy a noble title only because they had a child. After buying a piece of land, they had named it Snow Eagle Territory after their son!It was clear how much they loved him.
+
+
+
+Of course, this was only a small piece of land in Water Rites County.
+
+
+
+"I only started cultivating Qi when I was twenty years old, but my son is different. He’s only eight right now. I estimate that once he’s around ten years old he can begin cultivating Qi." Dong Bo Lie roared with laughter. "He will definitely surpass me." Dong Bo Lie looked at his son with fatherly love and expectation.
+
+
+
+"Considering what I’ve seen, ten years old sounds about right." The brawny lion-man also approved.
+
+
+
+With all their life experience, their judgement was surely accurate.
+
+
+
+"Father, you could actually throw a rock through a tree from so far away?" The boy stood next to the tree. He tried to hug it, but was unable to completely wrap his arms around it. Its trunk had a massive hole. "It would take forever for me to cut down a tree this thick."
+
+
+
+"Now you know the power of a Heaven Rank Knight," Tong San said. Dong Bo Lie chuckled. As a father, he still liked to show off in front of his son.
+
+
+
+"As powerful as a god?" The boy deviously grinned.
+
+
+
+"God?"
+
+
+
+Dong Bo Lie and the Tong San went silent.
+
+
+
+The Dragon Mountain Empire’s founder, the Dragon Mountain Emperor, was a formidable god. This was common knowledge, almost everyone in this world knew this. Dong Bo Lie could be considered a brave individual within the army, but compared to a god? There was simply no comparison.
+
+
+
+"It seems practicing 500 times is too little for you tonight. Mn. Let’s add one another 1000" Dong Bo Lie licked his lips.
+
+
+
+"Father!" The boy opened his eyes wide. "You, you..."
+
+
+
+"Looks like you still dare to argue with me! Remember, when arguing with your father, you will definitely suffer losses. Alright, now go back, go back." Dong Bo Lie said.
+
+
+
+Tong San took out a flute, put it in his mouth, and played a few low notes. The sound spread throughout the mountains.
+
+
+
+Soon, twenty armored soldiers rapidly rushed over.
+
+
+
+"Take the deer back." Dong Bo Lie commanded.
+
+
+
+"Yes, m’lord." The soldiers respectfully responded.
+
+
+
+Dong Bo Lie and Tong San brought Xue Ying to the peak of the mountain. A large number of horses and approximately 100 soldiers were gathered here. A mysterious purple-gowned lady was seated on a blanket atop the snow. Next to the purple-gowned lady was a child about two or three years old jumping around. The soldiers looked at the purple-gowned lady with reverence.
+
+
+
+This purple-gowned lady was a powerful mage!
+
+
+
+"Pebble, come look who’s here," the lady said. The two or three year old child immediately turned his head to look, eyes brightening.
+
+
+
+"Carry me, brother. Carry me, brother." The child turned around and ran over to Xue Ying.
+
+
+
+The lady also smiled at this scene.
+
+
+
+"Pebble." Xue Ying walked forward and crouched down. His younger brother Qing Shi threw himself into his embrace. "Carry me, brother. Carry me, brother!"
+
+
+
+Xue Ying carried his younger brother and kissed him.
+
+
+
+"Pebble, I hunted a deer today. Look." Xue Ying pointed at the deer the soldiers carried.
+
+
+
+"Veer? Veer?" Qing Shi opened his crow-black eyes wide as his mouth let out an unclear sound.
+
+
+
+Dong Bo Qing Shi was only two years old. Try as he might to speak, his words were still unclear and didn’t understand the meaning.
+
+
+
+"It’s a deer, a type of animal found on our family’s mountain."
+
+
+
+"Xue Ying, why don’t you give me your little brother." The purple-gowned lady also got up and walked over.
+
+
+
+"Yes, mother." Xue Ying handed over his little brother.
+
+
+
+The purple-robed lady said, "I brought some sweet olive cakes. They’re in the basket and should still be hot. Why don’t you hurry up and eat them?"
+
+
+
+"Cake?" Xue Ying’s eyes brightened and his mouth watered. Realizing that he was drooling, he immediately dashed over.
+
+
+
+"I want to eat! I want to eat!" Qing Shi immediately struggled from his mother’s embrace. He became very energetic whenever cake was mentioned. Actually, he was quite disobedient when eating.
+
+
+
+"Of course there’s some for you too, you little glutton." The purple-robed lady saw Dong Bo Lie and Tong San, "You two hurry up too. I made some food for you to eat as well."
+
+
+
+"Ha ha ... Master is not only powerful in magic, but also in the kitchen," said the brawny lion-man.
+
+
+
+Tong San had once been a slave when he was little, and had gone from being the purple-gowned lady’s servant to one of her followers. Even though many years had passed, and their relationship had grown to become like that of family, Tong San Insisted on calling her "Master."
+
+
+
+...
+
+
+
+Once Xue Ying was full, he looked into the distance. From the top of the mountain, he could see a few mountains and some farmland with a single glance. Everywhere he looked was his family’s territory. His mother and father had stopped their days of adventuring when he was born, and instead, purchased a title of nobility and a large piece of land. The entire strip of land had been titled Snow Eagle Territory!
+
+
+
+Dong Bo Xue Ying took a long stretch, his face full of happiness.
+
+
+
+He had a mother and father who loved him dearly. He had a cute little brother. He had the many kind people of his territory.
+
+
+
+With this type of life, Dong Bo Xue Ying was extremely satisfied.
+
+
+
+The only thing that gave him a headache was that his father’s training was too painful.
+
+
+
+"Practicing 1000 more times on top of the 500… and I still need to practice my spear techniques..." Dong Bo Xue Ying’s face turned sour.
+
+
+
+******
+
+
+
+Nightfall, under the waning moon.
+
+
+
+The wind whistled.
+
+
+
+Boom!
+
+
+
+A thousand meters above the ground, a giant bird flew quickly, resembling like a black cloud.
+
+
+
+This giant bird had four wings and a wingspan of over twenty meters. Its flying speed surpassed the speed of sound. It was the extremely tyrannical and terrible demon beast, the Four-Winged Vulture. Two figures sat cross-legged on its back. One was a man in silver armor, while the other was robed in grey and carried a purple staff.
+
+
+
+"Where are we?" asked the grey-robed person.
+
+
+
+"Master, we have already entered Water Rites County. We should arrive at Snow Eagle Territory in about half an hour." The silver-armoured man lowered his head, clearly distinguishing his lower position.
+
+
+
+"I’ll finally see my sister again in half an hour." The grey-robed person’s voice carried heavy emotions. "You really are quite capable of hiding. Even when pursued by our family, you were able to hide for more than fifteen years..."
+
+
+
+In the darkness of the night, the Four-Winged Vulture headed straight for Snow Eagle Territory.
+  			</p>
+  		</div>
+  		<div id="main-center">
+  			<p>If you want to get the newest update, you can follow us</p>
+  			<div id="twitter">
+  				<h3>MeeJee Team</h3>
+  				<button id="lest-button">follow</button>
+  			</div>
+  			<div id="facebook">
+  				<h3>@MeeJee Team</h3>
+  				<button id="f-button">follow</button>
+  			</div>
+  			<!--邮箱-->
+  			<div id="mail">
+  				<p>Or you can leave your Email here,we will sent the newest chapter to you when the book is update</p>
+  				<input type="text" id="text"/>
+  				<div id="submit"><span>subscibe</span></div>
+  			</div>
+  		</div>
+  		<div id="main-down">
+  			<div id="like">
+  				<h2>Maybe you will like:</h2>
+  			</div>
+  			<div id="cover">
+  				<div id="cover-item"><img src="../../static/images/1516613561(1).jpg"/></div>
+  				<div id="cover-item"><img src="../../static/images/1516613561(1).jpg"/></div>
+  				<div id="cover-item"><img src="../../static/images/1516613561(1).jpg"/></div>
+  			</div>
+  			<div id="cover">
+  				<div id="cover-item"><img src="../../static/images/1516613561(1).jpg"/></div>
+  				<div id="cover-item"><img src="../../static/images/1516613561(1).jpg"/></div>
+  				<div id="cover-item"><img src="../../static/images/1516613561(1).jpg"/></div>
+  			</div>
+  			<div id="cover">
+  				<div id="cover-item"><img src="../../static/images/1516613561(1).jpg"/></div>
+  				<div id="cover-item"><img src="../../static/images/1516613561(1).jpg"/></div>
+  				<div id="cover-item"><img src="../../static/images/1516613561(1).jpg"/></div>
+  			</div>
+  		</div>
+  	</div>
+  	<div id="footer">
+  		<div id="footer-face"></div>
+  		<div id="footer-center"><span>MeeJee Team</span><br><span>Get the updates of the stories</span></div>
+  		<button id="f-button1">follow</button>
+  	</div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data:function(){  
+        return {  
+            isShow:false,
+            iscatalog:false
+        };  
+    },
+	 methods:{
+         toggle:function(){
+         this.isShow = !this.isShow;
+         this.iscatalog = false;
+         },
+         catalog:function(){
+         	this.iscatalog = !this.iscatalog;
+         }
+		} 
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+#wrap{
+	height: 100%;
+}
+#header{
+	width: 100%;
+	height:1.263888rem;
+	/*background: black;*/
+	border-bottom:2px solid black;
+	line-height: 1.263888rem;
+	text-indent: 0.555555rem;
+	font-family: Times New Roman;
+}
+/**/
+#header-next{
+	width: 8.333333rem;
+	/*height: 8.333333rem;*/
+	margin: auto;
+	text-align: center;
+}
+#novel-cover{
+	width: 2.777777rem;
+	height: 2.777777rem;
+	padding-top: 0.477777rem;
+	margin:auto;
+}
+#novel-cover img{
+	width: 2.777777rem;
+	height: 2.777777rem;
+}
+#novel-cover h3{
+	margin-top: 2.277777rem;
+	padding-top: 0.477777rem;
+}
+#h3{
+	height: 0.666666rem;
+	margin-top: 1rem;
+}
+#author{
+	height: 0.666666rem;
+	font-size: 0.347222rem;
+	font-family: Georgia;
+	line-height: 0.666666rem;
+}
+#introduce{
+	text-align: center;
+	word-wrap:break-word;
+	margin-top: 0.277777rem;
+}
+/*目录*/
+#Catalog{
+	height: 1.116666rem;
+	line-height: 1.116666rem;
+	text-indent: 0.2222222rem;
+	border-top: 1px solid #000000;
+	margin-top: 0.816666rem;
+}
+/*内容*/
+#main-wrap{
+	height: 14.77777rem;
+	/*background: black;*/
+	overflow-y: scroll;
+	position: absolute;
+}
+/*阅读*/
+#main-up{
+	width: 90%;
+	margin: auto;
+	text-align: left;
+}
+#main-up p{
+	text-indent: 0.277777rem;
+}
+/*推广*/
+#main-center{
+	height: 7.66666rem;
+	text-align: center;
+	border-top: 1px solid black;
+	border-bottom: 1px solid black;
+}
+#main-center p{
+	margin-top: 0.4444444rem;
+	font-family: Georgia;
+}
+/**/
+#twitter{
+	height: 1.33333rem;
+	line-height: 1.33333rem;
+	background: url(../../static/images/twitter.png)no-repeat;
+	background-position-x: 0.555555rem;
+	background-position-y: 50%;
+	background-size:0.99999rem 0.99999rem;
+}
+#twitter h3{
+	text-indent: 2.3333333rem;
+	float: left;	
+}
+#facebook{
+	height: 1.33333rem;
+	line-height: 1.33333rem;
+	background: url(../../static/images/facebook.png)no-repeat;
+	background-position-x: 0.555555rem;
+	background-position-y: 50%;
+	background-size:0.99999rem  0.99999rem;
+}
+#facebook h3{
+	text-indent: 2.3333333rem;
+	float: left;	
+}
+#f-button{
+	float: right;
+	margin-top: 0.2222rem;
+	margin-right:.5rem;
+	width: 1.7777rem;
+	height: 0.88888rem;
+	outline: none;	
+	font-family: Georgia;
+}
+#lest-button{
+	float: right;
+	margin-top: 0.2222rem;
+	margin-right:.5rem;
+	width: 1.7777rem;
+	height: 0.88888rem;
+	outline: none;
+	font-family: Georgia;
+}
+#f-button1{
+	float: right;
+	margin-top: 0.3333rem;
+	margin-right:.5rem;
+	width: 1.7777rem;
+	height: 0.88888rem;
+	outline: none;
+	font-family: Georgia;
+}
+/*邮箱*/
+#mail p{
+	text-indent: 0.388888rem;
+	font-family: Georgia;
+}
+#text{
+	width: 6.166666rem;
+	height: 0.516666rem;
+	margin-top: 0.33333rem;
+	border: 1px solid #000000;
+	outline: none;
+}
+#submit{
+	width: 2.33333rem;
+	height: 0.77777rem;
+	margin: auto;
+	margin-top: 0.277777rem;
+	text-align: center;
+	line-height: 0.77777rem;
+	border: 1px solid black;
+	border-radius:3px ;
+}
+/*推荐*/
+/**/
+#main-down{
+	/*height: 13.52222rem;*/
+	/*height: 100%;*/
+	/*@media screen and（max-width：300px）;*/
+}
+
+#like{
+	height:1.333333rem;
+	line-height: 1.333333rem;
+	text-indent: 0.388888rem;
+}
+#cover{
+	width: 9.22222rem;
+	height: 2.888888rem;
+	margin: auto;
+}
+#cover-item:nth-of-type(1){
+	margin-left: 0.277777rem;
+}
+#cover-item{
+	float: left;
+	margin-left: 0.416666rem;
+}
+#cover-item img{
+	width: 2.632rem;
+	height:2.632rem;
+}
+
+/*底部*/
+#footer{
+	width: 100%;
+	height: 1.763888rem;
+	border: 1px solid #3a569f;
+	background: #FFFFFF;
+	position: absolute;
+	bottom: 0;
+	left: 0;
+}
+#footer-face{
+	width:2rem;
+	height: 2rem;
+	background: url(../../static/images/facebook.png)no-repeat;
+	background-position-x: 0.416666rem;
+	background-position-y:0.21111111rem ;
+	float: left;
+}
+#footer-center{
+	width: 4.777777rem;
+	height: 1.294444rem;
+	margin-top: 0.377777rem;
+	float: left;
+}
+#footer-center span:nth-of-type(1){
+	font-size: 0.416666rem;
+	font-weight: bold;
+}
+/**/
+
+/*弹窗*/
+#Popup{
+	width: 100%;
+	height: 1.388888rem;
+	position: absolute;
+	background: #FFFFFF;
+	z-index: 99;
+	top: 1.266666rem;
+	left: 0;
+	display: flex;
+	justify-content: space-around;
+	border-bottom: 1px solid #000000;
+}
+#previous{
+	width: 2.222222rem;
+	height: 0.833333rem;
+	text-align: center;
+	line-height:  0.833333rem;
+	border: 1px solid black;
+	border-radius:3px;
+	margin-top: 0.277777rem;
+}
+#next{
+	width: 2.222222rem;
+	height: 0.833333rem;
+	text-align: center;
+	line-height:  0.833333rem;
+	border: 1px solid black;
+	border-radius:3px;	
+	margin-top: 0.277777rem;
+}
+#menu{
+	width: 2.222222rem;
+	height: 0.833333rem;
+	text-align: center;
+	line-height:  0.833333rem;
+	border: 1px solid black;
+	border-radius:3px;	
+	margin-top: 0.277777rem;
+}
+/*目录弹窗*/
+#Catalog-p{
+	width: 100%;
+	height: 2.777777rem;
+	background: #FFFFFF;
+	position: absolute;
+	top: 2.6333rem;
+	left: 0;
+	border-bottom: 1px solid black;
+	border-top: 1px solid black;
+	z-index: 32;
+}
+#Catalog-p p{
+	line-height: 0.634444rem;
+	text-indent: 0.277777rem;
+}
+</style>
